aac811579ac87e9fa2f4a93bb38842642ce8b77c
syrine12345 <syrineb19@gmail.com>
2021-05-16 14:16:04 +0100

    Add upload photo button

--- car-rental/src/app/menu-profile/menu-profile.component.ts
+++ car-rental/src/app/menu-profile/menu-profile.component.ts
@@ -1,15 +1,25 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-menu-profile',
   templateUrl: './menu-profile.component.html',
   styleUrls: ['./menu-profile.component.css']
 })
-export class MenuProfileComponent implements OnInit {
+export class MenuProfileComponent   {
 
-  constructor() { }
+  url: any = '';
+  
+  onSelectFile(event) {
+    if (event.target.files && event.target.files[0]) {
+      var reader = new FileReader();
 
-  ngOnInit(): void {
+      reader.readAsDataURL(event.target.files[0]); // read file as data url
+
+      reader.onload = (event) => { // called once readAsDataURL is completed
+        this.url = event.target.result;
+      }
+    }
   }
+
 
 }
