6f8d3646d6495349b2262d65e5cb71817276290d
Haythem Ben Salah <haythem.bensalah@insat.u-carthage.tn>
2021-06-12 22:43:42 +0100

    modified 2

--- car-rental/src/app/app-routing.module.ts
+++ car-rental/src/app/app-routing.module.ts
@@ -3,13 +3,26 @@ import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import {CategoriesComponent} from './categories/categories.component';
 import { AboutAsComponent} from './about-as/about-as.component';
+import { ProfileComponent } from './profile/profile.component';
+import { AccountSettingsProfileComponent } from './account-settings-profile/account-settings-profile.component';
+import { SecurityProfileComponent } from './security-profile/security-profile.component';
+import { HistoryProfileComponent } from './history-profile/history-profile.component';
+import { InfoMenuProfileComponent } from './info-menu-profile/info-menu-profile.component';
+import { NotificationProfileComponent } from './notification-profile/notification-profile.component';
 
 
 const routes: Routes = [
   {path : '' , redirectTo : '/home', pathMatch : 'full'},
   {path : 'home' , component : HomeComponent},
   {path : 'categories',component: CategoriesComponent},
-  {path :'about_as' , component : AboutAsComponent}
+  {path :'about_as' , component : AboutAsComponent},
+  { path: 'profile', component: ProfileComponent, children: [
+    { path: 'settings', component: AccountSettingsProfileComponent },
+    { path: 'security', component: SecurityProfileComponent },
+    { path: 'history', component: HistoryProfileComponent },
+    { path: 'profileinformation', component: InfoMenuProfileComponent },
+    { path: 'notifications', component: NotificationProfileComponent }
+]}
 ];
 
 @NgModule({
