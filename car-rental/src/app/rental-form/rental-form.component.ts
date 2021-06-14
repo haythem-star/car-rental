954c0335287e95257d0c2ddcad803b9426086eba
Haythem Ben Salah <haythem.bensalah@insat.u-carthage.tn>
2021-06-15 00:11:10 +0100

    work on payement

--- car-rental/src/app/rental-form/rental-form.component.ts
+++ car-rental/src/app/rental-form/rental-form.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Data } from '@angular/router';
 
 @Component({
   selector: 'app-rental-form',
@@ -6,10 +7,28 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./rental-form.component.css']
 })
 export class RentalFormComponent implements OnInit {
+  imgPath :string;
+  start_rental : Date ;
+  end_rental : Date;
+  durations : string;
+  model : string;
+  price : number;
+  sessionId ;
 
-  constructor() { }
+
+  constructor(private route : ActivatedRoute) { }
 
   ngOnInit(): void {
+    this.route.data.subscribe((data : Data) => {
+      this.imgPath = data['service'].car.imgPath;
+      this.start_rental = data['service'].start_rental;
+      this.end_rental = data['service'].end_rental;
+      this.durations = data['service'].durations;
+      this.model = data['service'].car.model;
+      this.price = data['service'].price;
+      this.sessionId = data['service'].sessionId;
+    })
+
   }
 
 }
